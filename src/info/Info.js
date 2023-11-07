09297e5bb0fae99c78182da3285663a4f9f02828
Matheus <matheus@Matheuss-MacBook-Pro.local>
2023-11-07 11:43:04 -0300

    first commit

--- src/info/Info.js
+++ src/info/Info.js
@@ -1,88 +1,57 @@
-import self from "../img/self.png"
-import mock1 from "../img/mock1.png"
-import mock2 from "../img/mock2.png"
-import mock3 from "../img/mock3.png"
+import self from "../img/self.gif"
+import node from "../img/node-project.webp"
+import pricenuxt from "../img/pricenuxt.png"
+import petshop from "../img/pet.png"
 import mock4 from "../img/mock4.png"
 import mock5 from "../img/mock5.png"
+import prismapost from "../img/prisma-ts-nest.png"
 
-/* Hi there! Thanks for checking out my portfolio template. Be sure to read the comments to get a better understanding of
-how to make this template work best for you! */
-
-export let colors = ["rgb(0,255,164)", "rgb(166,104,255)"];
-/*
-I highly recommend using a gradient generator like https://gradientgenerator.paytonpierce.dev/ to generate a pair of colors that you like.
-These colors will be used to style your name on the homepage, the background of your picture, and some other accents throughout
-the site.
- */
-
-
-/*
-So let's get started! Some of the info below is pretty self-explanatory, like 'firstName' and 'bio'. I'll try to explain anything
-that might not be obvious right off the bat :) I recommend looking at the template example live using "npm start" to get an idea
-of what each of the values mean.
- */
+export let colors = ["#a8a927", "#485d18"];
 
 export const info = {
-    firstName: "John",
-    lastName: "Smith",
-    initials: "js", // the example uses first and last, but feel free to use three or more if you like.
+    firstName: "Matheus",
+    lastName: "Patrick",
+    initials: "m", 
     position: "a Full Stack Developer",
-    selfPortrait: self, // don't change this unless you want to name your self-portrait in the "img" folder something else!
-    gradient: `-webkit-linear-gradient(135deg, ${colors})`, // don't change this either
+    selfPortrait: self,  
+    gradient: `-webkit-linear-gradient(135deg, ${colors})`, 
     baseColor: colors[0],
-    miniBio: [ // these are just some "tidbits" about yourself. You can look at mine https://paytonjewell.github.io/#/ for an example if you'd like
+    miniBio: [ 
         {
             emoji: '☕',
             text: 'fueled by coffee'
         },
         {
-            emoji: '🌎',
-            text: 'based in the US'
+            emoji: '🇧🇷',
+            text: 'based in the BR',
         },
         {
             emoji: "💼",
-            text: "Systems Engineer at Google"
+            text: "full stack developer at ezok"
         },
         {
             emoji: "📧",
-            text: "johnsmith@gmail.com"
+            text: "mathtg2@gmail.com"
         }
     ],
     socials: [
         {
-            link: "https://facebook.com",
-            icon: 'fa fa-facebook',
-            label: 'facebook'
-        },
-        {
-            link: "https://instagram.com",
-            icon: 'fa fa-instagram',
-            label: 'instagram'
-        },
-        {
-            link: "https://github.com",
+            link: "https://github.com/matheustavaresz",
             icon: "fa fa-github",
             label: 'github'
         },
         {
-            link: "https://linkedin.com",
+            link: "https://www.linkedin.com/in/matheus-patrickz/",
             icon: "fa fa-linkedin",
             label: 'linkedin'
-        },
-        {
-            link: "https://twitter.com",
-            icon: "fa fa-twitter",
-            label: 'twitter'
         }
-// Feel free to remove any of these that you don't have. I'll write an FAQ on how to add new ones later, let me know if you have a request for an icon!
-// Just change the links so that they lead to your social profiles.
 
     ],
-    bio: "Hello! I'm John. I'm a systems engineer for Google. I studied CompSci at Harvard, I enjoy long walks on the beach, and I believe artificial intelligence will inevitably rule us all one day. You should hire me!",
+    bio: "Hello! My name is Patrick, and I'm a web developer at Ezok. I'm currently studying Data Science at Descomplica. I enjoy long walks on the beach and hold the belief that artificial intelligence will inevitably govern our future. I would be a valuable addition to your team!",
     skills:
         {
-            proficientWith: ['javascript', 'react', 'git', 'github', 'bootstrap', 'html5', 'css3', 'figma'],
-            exposedTo: ['nodejs', 'python', 'adobe illustrator']
+            proficientWith: ['javascript', 'typescript', 'react', 'vue', 'nuxt', 'nest', 'aws', 'bootstrap', 'html5', 'css3'],
+            exposedTo: ['nodejs', 'docker', 'angular', 'azure']
         }
     ,
     hobbies: [
@@ -91,8 +60,8 @@ export const info = {
             emoji: '📖'
         },
         {
-            label: 'theater',
-            emoji: '🎭'
+            label: 'games',
+            emoji: '🎮'
         },
         {
             label: 'movies',
@@ -102,39 +71,29 @@ export const info = {
             label: 'cooking',
             emoji: '🌶'
         }
-// Same as above, change the emojis to match / relate to your hobbies or interests.
-// You can also remove the emojis if you'd like, I just think they look cute :P
     ],
-    portfolio: [ // This is where your portfolio projects will be detailed
+    portfolio: [ 
         {
-            title: "Project 1",
-            live: "https://paytonpierce.dev", //this should be a link to the live version of your project, think github pages, netlify, heroku, etc. Or your own domain, if you have it.
-            source: "https://github.com/paytonjewell", // this should be a link to the **repository** of the project, where the code is hosted.
-            image: mock1
+            title: "Node Scraper",
+            source: "https://github.com/MatheusTavaresz/node-scraper/", 
+            image: node
         },
         {
-            title: "Project 2",
-            live: "https://paytonpierce.dev",
-            source: "https://github.com/paytonjewell",
-            image: mock2
+            title: "Course CRUD NestJS",
+            source: "https://github.com/MatheusTavaresz/NestJS-CourseCRUD",
+            image: prismapost, 
         },
         {
-            title: "Project 3",
-            live: "https://paytonpierce.dev",
-            source: "https://github.com/paytonjewell",
-            image: mock3
+            title: "Nuxt Scraper",
+            live: "https://price-search-90rkul9a1-matheustavaresz.vercel.app/",
+            source: "https://github.com/MatheusTavaresz/price-search-nuxt",
+            image: pricenuxt
         },
         {
-            title: "Project 4",
-            live: "https://paytonpierce.dev",
-            source: "https://github.com/paytonjewell",
-            image: mock4
-        },
-        {
-            title: "Project 5",
-            live: "https://paytonpierce.dev",
-            source: "https://github.com/paytonjewell",
-            image: mock5
+            title: "Landing Page Petshop",
+            live: "https://desafio-petshop.vercel.app/",
+            source: "https://github.com/MatheusTavaresz/desafio-petshop",
+            image: petshop
         }
     ]
 }
